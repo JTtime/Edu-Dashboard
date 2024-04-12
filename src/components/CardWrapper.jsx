a7533a5942f8ca442b59520bcc23d8eaec45874b
JTtime <jeevraj.vjti@gmail.com>
2024-04-12 10:27:08 +0530

    navbar styling

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function CardWrapper ({children}) {
     return (
         <>
-        <Box sx={{border: '2px solid blue', borderRadius: '8px', padding: '1rem', height: '100%'}}>
+        <Box sx={{border: '2px solid blue', borderRadius: '8px', padding: '1rem', height: '100%', backgroundColor: 'white'}}>
             {children}
         </Box>
         </>
